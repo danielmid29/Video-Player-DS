b77597284d1cd90657281cb7bee4b45554852767
unknown <you@example.com>
2024-06-30 11:55:59 +0530

    Reapply "commit"
    This reverts commit be84e4ff47bb44fb6b2d293c9ec148a45db6553e.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:video_player_ds/Components/VideoPlayerComp.dart';
 
-import 'Components/Loading.dart';
+import 'Components/loading.dart';
 
-void main()  {
-    WidgetsFlutterBinding.ensureInitialized();
-    runApp( Shortcuts(
-      shortcuts: <LogicalKeySet,Intent>{
-        LogicalKeySet(LogicalKeyboardKey.select):const ActivateIntent(),
-      },
-      child: const MaterialApp(
-        home: Loading(),
-        debugShowCheckedModeBanner: false,
-        // home: VideoPlayerSync(),
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Player Demo',
+      debugShowCheckedModeBanner: false,
+      home: Loading(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-    ));
+      body: Center(
+        child: Loading(), // Replace with your loading widget
+      ),
+    );
+  }
 }
